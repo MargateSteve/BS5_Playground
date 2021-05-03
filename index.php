addb83259fc10c563f67723b8773e6f9d1e61e78
MargateSteve <MargateSteve@users.noreply.github.com>
2021-05-03 22:19:12 +0100

    All Bootstrap examples implemented
    Updated all bootstrap examples to use local versions of CSS and JS.
    
    Added images from Bootstrap Examples page to create alternative version of that as our links.

--- index.php
+++ index.php
@@ -8,9 +8,6 @@
         <meta name="generator" content="Hugo 0.82.0">
         <title>BS5 Playground</title>
 
-
-
-
         <!-- Bootstrap core CSS -->
         <link href="assets/css/bootstrap.css" rel="stylesheet">
 
@@ -32,29 +29,36 @@
             }
             ?>
             <h5>Default Bootstrap Examples</h5>
-            <?php 
-            foreach ($result as $key => $value) {
-                $parts = explode('/',$value);
-                if($parts[1] !== 'assets') {
-                    $name = str_replace('-', ' ', $parts[1]);
-                    $name = str_replace('rtl', ' RTL', $name);
-                    $expectedPosition = strlen($name) - strlen('RTL');
-                    if($key==0) {
-                        echo '<a href="'.$value.'" class="text-decoration-none">'.ucwords($name).'</a>';
-                    } else {
-                        if(strrpos($name, 'RTL', 0) === $expectedPosition) {
-                        echo ' <a href="'.$value.'" class="ms-4 text-decoration-none">RTL</a>';
-                    } else {
-                        echo '<br><a href="'.$value.'" class=" text-decoration-none">'.ucwords($name).'</a>';
-                    }
-                    }
-                    
+            <div class="row">
+                <?php 
+                foreach ($result as $key => $value) {
+                    $parts = explode('/',$value);
+ 
+                    if($parts[1] !== 'assets') {
+                        
+                        $is_rtl = strpos($parts[1], '-rtl');
+                        $name = str_replace('-', ' ', $parts[1]);
 
+                        if(!$is_rtl) {
+                            echo '<div class="col-sm-6 col-md-4 col-xl-3 mb-3">';
+
+                            echo '<img class="img-thumbnail mb-3" src="assets/img/example_thumbs/'.$parts[1].'.png" alt="" width="240" height="150" loading="lazy">';
+                            echo '<h3 class="h5 mb-1"><a class="text-decoration-none" href="'.$value.'" class="text-decoration-none">'.ucwords($name).'</a>';
+                            
+                            if(!is_dir('examples/'.$name.'-rtl')) {
+                               echo '</h3></div>';
+                            }
+                            
+                        } else {
+                            echo '<a href="'.$value.'" class="text-decoration-none float-end">RTL</a>';
+                            echo '</h3></div>';
+                            
+                        }
+                    }
                 }
-
-            }
-            ?>
-
+                
+                ?>
+            </div><!-- row -->    
             <footer class="pt-5 my-5 text-muted border-top">
                 Created by the Bootstrap team &middot; &copy; 2021
             </footer>
